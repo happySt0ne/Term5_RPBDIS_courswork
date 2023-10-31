bfbe6bacb1e550326683d093da0e90109074f209
H1Kt0 <konovalchuk.denis@gmail.com>
2023-10-31 22:16:26 +0300

    Добавил методы просмотра, удаления, добавления и обновления данных.

--- Term5_RPBDIS_coursework/Term5_RPBDIS_lab2_console/Program.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_lab2_console/Program.cs
@@ -1,30 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.VisualBasic;
-using System.Collections;
-using System.Threading.Channels;
-using Term5_RPBDIS_library;
+﻿using Term5_RPBDIS_library;
 using Term5_RPBDIS_library.models.tables;
 
 class Program {
     static void Main(string[] args) {
-        CourseworkContext context = new();
-        context.Marks.ToList().ForEach(x => Console.WriteLine(x.Value));
-
-
+        Task10();
     }
 
     private static void Task1() {
-        CourseworkContext context = new();
+        ValuatingSystemContext context = new();
 
         context.Achievements
             .Select(x => new { x.AchievementId, x.Text })
             .ToList()
             .ForEach(x => Console.WriteLine($"Id = {x.AchievementId}; text = {x.Text}"));
+
     }
 
     private static void Task2() {
-        CourseworkContext context = new();
+        ValuatingSystemContext context = new();
 
         context.Marks
             .Where(x => x.Value > 50)
@@ -34,7 +27,7 @@ class Program {
     }
 
     private static void Task3() {
-        CourseworkContext context = new();
+        ValuatingSystemContext context = new();
 
         var a = context.Employees
             .OrderByDescending(x => x.Mark)
@@ -46,7 +39,7 @@ class Program {
     }
 
     private static void Task4() {
-        CourseworkContext context = new();
+        ValuatingSystemContext context = new();
 
         context.Employees
             .Select(x => new { Name = x.Name, DivisionName = x.Division.Name })
@@ -55,7 +48,7 @@ class Program {
     }
 
     private static void Task5() {
-        CourseworkContext context = new();
+        ValuatingSystemContext context = new();
 
         context.Employees
             .Where(x => x.Mark.Value % 2 == 0)
@@ -66,17 +59,42 @@ class Program {
     }
 
     private static void Task6() {
-        CourseworkContext context = new();
+        ValuatingSystemContext context = new();
+
+        var lastAchieve = context.Achievements
+            .OrderBy(a => a.AchievementId)
+            .Select(a => new { a.AchievementId, a.Text })
+            .LastOrDefault();
+
+        Console.WriteLine($"Last record in database before adding:\n{lastAchieve}");
 
         Achievement achievement = new() { Text = "compliting 6 task in lab 2)" };
         context.Achievements.Add(achievement);
         context.SaveChanges();
 
-        context.Achievements.ToList().ForEach(x => Console.WriteLine(x.Text));
+        lastAchieve = context.Achievements
+            .OrderBy(a => a.AchievementId)
+            .Select(a => new { a.AchievementId, a.Text })
+            .LastOrDefault();
+
+        Console.WriteLine($"Last record in database after adding:\n{lastAchieve}");
     }
 
     private static void Task7() {
-        CourseworkContext context = new();
+        ValuatingSystemContext context = new();
+
+        var lastRecord = context.Employees
+            .OrderBy(e => e.EmployeeId)
+            .Select(x => new {
+                x.EmployeeId,
+                x.Name,
+                x.HireDate,
+                x.Mark.Value,
+                x.Achievement.Text,
+                divisionName = x.Division.Name})
+            .LastOrDefault();
+
+        Console.WriteLine($"Last record in database before adding:\n{lastRecord}");
 
         Employee employee = new() {
             Division = context.Divisions.FirstOrDefault(d => d.DivisionId == 4),
@@ -89,7 +107,8 @@ class Program {
         context.Employees.Add(employee);
         context.SaveChanges();
 
-        context.Employees
+        lastRecord = context.Employees
+            .OrderBy(e => e.EmployeeId)
             .Select(x => new {
                 x.EmployeeId,
                 x.Name,
@@ -98,9 +117,67 @@ class Program {
                 x.Achievement.Text,
                 divisionName = x.Division.Name
             })
-            .OrderBy(x => x.EmployeeId)
+            .LastOrDefault();
+
+        Console.WriteLine($"\nLast record in database after adding:\n{lastRecord}");
+    }
+
+    private static void Task8() {
+        ValuatingSystemContext context = new();
+
+        var lastRecord = context.Achievements
+            .OrderBy(a => a.AchievementId)
+            .LastOrDefault();
+        Console.WriteLine($"Last record before deleting:\nId = {lastRecord.AchievementId}; Text = {lastRecord.Text}");
+
+        context.Achievements.Remove(lastRecord);
+        context.SaveChanges();
+
+        lastRecord = context.Achievements
+            .OrderBy(a => a.AchievementId)
+            .LastOrDefault();
+        Console.WriteLine($"Last record after deleting:\nId = {lastRecord.AchievementId}; Text = {lastRecord.Text}");
+
+    }
+
+    private static void Task9() {
+        ValuatingSystemContext context = new();
+
+        var lastRecord = context.Employees
+            .OrderBy(e => e.EmployeeId)
+            .LastOrDefault();
+
+        Console.WriteLine("Last record in database before deleting:\nId=" +
+            lastRecord.EmployeeId + " Name = " + lastRecord.Name);
+
+        context.Employees.Remove(lastRecord);
+        context.SaveChanges();
+
+        lastRecord = context.Employees
+            .OrderBy(e => e.EmployeeId)
+            .LastOrDefault();
+
+        Console.WriteLine("\nLast record in database after deleting:\nId=" +
+            lastRecord.EmployeeId + " Name = " + lastRecord.Name);
+    }
+
+    private static void Task10() {
+        ValuatingSystemContext context = new();
+
+        var listToUpdate = context.Achievements.Where(x => x.AchievementId % 2 == 0);
+
+        if (listToUpdate is null) return;
+
+        foreach (var item in listToUpdate) {
+            item.Text = item.Text + "☺";
+        }
+
+        context.SaveChanges();
+
+        context.Achievements
+            .Where(x => x.AchievementId % 2 == 0)
+            .Select(x => new { x.AchievementId, x.Text })
             .ToList()
             .ForEach(Console.WriteLine);
-
     }
 }
