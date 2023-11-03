061bd30a7a7d882e91a8a483f07566ca990e2a85
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-03 19:44:19 +0300

    Добавил класс ``Middlewares`` для хранения обработчиков запросов. Также добавил классы-пустышки для будущих сервисов кэширования.

---
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_mainLogic/Middlewares.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace Term5_RPBDIS_mainLogic {
+    public class Middlewares {
+        public static void GetClientInfo(IApplicationBuilder app) {
+            app.Run(async context => {
+                string answer = "";
+                answer += "remote ip:\n";
+                answer += context.Connection.RemoteIpAddress + "\n\n";
+
+                answer += "client language:\n";
+                answer += context.Request.Headers["Accept-Language"] + "\n\n";
+
+                answer += "client browser info:\n";
+                answer += context.Request.Headers["User-Agent"] + "\n\n";
+
+                await context.Response.WriteAsync(answer);
+            });
+        }
+    }
+}
