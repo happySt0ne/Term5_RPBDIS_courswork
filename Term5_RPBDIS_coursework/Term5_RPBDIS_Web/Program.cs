60aedb9365ac7427caf24e43296d2282e83c1040
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-04 16:41:42 +0300

    Добавил сервис кеширования и проверил его работоспособность.

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Program.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Program.cs
@@ -1,11 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.IdentityModel.Tokens;
 using System.Reflection.Metadata;
+using Term5_RPBDIS_library;
+using Term5_RPBDIS_library.models.tables;
 using Term5_RPBDIS_mainLogic;
+using Term5_RPBDIS_mainLogic.Services;
+
+const int CACHE_TIME_SECONDS = 264;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ValuatingSystemContext>(options => options.UseSqlServer(connection));
+builder.Services.AddTransient<AchievementService>();
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddTransient(x => 
+    new MemoryCacheEntryOptions {
+        AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(CACHE_TIME_SECONDS),
+    }
+);
 
 var app = builder.Build();
 
@@ -26,5 +44,11 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Map("/info", Middlewares.GetClientInfo);
+
+app.Run(context => {
+    var a = context.RequestServices.GetService<AchievementService>();
+    var b = a?.GetAchievements("Achievements20");
+    return context.Response.WriteAsync(b.FirstOrDefault().Text);
+});
 
 app.Run();
--- Term5_RPBDIS_coursework/Term5_RPBDIS_mainLogic/Services/AchievementService.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_mainLogic/Services/AchievementService.cs
@@ -1,11 +1,42 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Term5_RPBDIS_library;
+using Term5_RPBDIS_library.models.tables;
 
 namespace Term5_RPBDIS_mainLogic.Services {
     public class AchievementService {
+        private ValuatingSystemContext _valuatingSystemContext;
+        private IMemoryCache _cache;
+        private const int ROWS_NUMBER = 20;
+        private readonly IServiceProvider _serviceProvider;
+
+        public AchievementService(ValuatingSystemContext context, IMemoryCache memoryCache, IServiceProvider service) {
+            _valuatingSystemContext = context;
+            _cache = memoryCache;
+            _serviceProvider = service;
+        }
+
+        public IEnumerable<Achievement>? GetAchievements(string cacheKey) {
+            if (_cache.TryGetValue(cacheKey, out IEnumerable<Achievement> achievements)) {
+
+                return achievements;
+            }
+
+            achievements = _valuatingSystemContext.Achievements.OrderBy(x => x.AchievementId).Take(ROWS_NUMBER).ToList();
+
+            if (achievements is not null) {
+
+                _cache.Set(cacheKey, achievements, _serviceProvider.GetService<MemoryCacheEntryOptions>());
+            }
+
+            return achievements;
+        }
 
     }
 }
