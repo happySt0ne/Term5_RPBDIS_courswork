bbdab124437269e714defff76bd9617a63e55124
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-11 14:45:38 +0300

    Разделил класс `Middlewares` на более мелкие и контекстно понятные классы.

---
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_mainLogic/Middlewares/Info.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Term5_RPBDIS_mainLogic {
+    public static partial class Middlewares {
+        public class Info {
+            public static void GetClientInfo(IApplicationBuilder app) {
+                app.Run(async context => {
+                    string answer = "";
+                    answer += "remote ip:\n";
+                    answer += context.Connection.RemoteIpAddress + "\n\n";
+
+                    answer += "client language:\n";
+                    answer += context.Request.Headers["Accept-Language"] + "\n\n";
+
+                    answer += "client browser info:\n";
+                    answer += context.Request.Headers["User-Agent"] + "\n\n";
+
+                    await context.Response.WriteAsync(answer);
+                });
+            }
+        }
+    }
+}
