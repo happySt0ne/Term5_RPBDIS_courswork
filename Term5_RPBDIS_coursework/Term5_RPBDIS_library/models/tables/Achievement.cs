7ff283315c8c698a616eee8c3809de11c0f21e2f
Денис Коновальчук <103820240+happySt0ne@users.noreply.github.com>
2023-11-13 00:15:04 +0300

    Revert "execution lab3 (#3)"
    This reverts commit 20c00cc14ef81303f54848140b48ed3e91ab0c90.

--- Term5_RPBDIS_coursework/Term5_RPBDIS_library/models/tables/Achievement.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_library/models/tables/Achievement.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using Term5_RPBDIS_sql_library;
 
 namespace Term5_RPBDIS_library.models.tables;
 
-public partial class Achievement : ISqlTable
+public partial class Achievement
 {
     public int AchievementId { get; set; }
 
     public string? Text { get; set; }
 
     public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
-
-    public int ID => AchievementId;
 }
--- Term5_RPBDIS_coursework/Term5_RPBDIS_library/models/tables/Mark.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_library/models/tables/Mark.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using Term5_RPBDIS_sql_library;
 
 namespace Term5_RPBDIS_library.models.tables;
 
-public partial class Mark : ISqlTable
+public partial class Mark
 {
     public int MarkId { get; set; }
 
@@ -13,6 +12,4 @@ public partial class Mark : ISqlTable
     public virtual ICollection<Division> Divisions { get; set; } = new List<Division>();
 
     public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
-
-    public int ID => MarkId;
 }
