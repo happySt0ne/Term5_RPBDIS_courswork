6241f9da62539a3ee56dbc2ab4586b416d07a93f
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-19 18:47:23 +0300

    Переделал получение информации о клиенте с учётом использования `MVC`.

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/HomeController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/HomeController.cs
@@ -2,7 +2,15 @@
 
 namespace Term5_RPBDIS_Web.Controllers {
     public class HomeController : Controller {
-        public IActionResult Index() {
+        public IActionResult Index() => View();
+
+        [Route("/info")]
+        [Route("/home/info")]
+        public IActionResult Info() {
+            ViewBag.RemoteIp = HttpContext.Connection.RemoteIpAddress;
+            ViewBag.ClientLanguage = HttpContext.Request.Headers["Accept-Language"];
+            ViewBag.ClientBrowser = HttpContext.Request.Headers["User-Agent"];
+
             return View();
         }
     }
