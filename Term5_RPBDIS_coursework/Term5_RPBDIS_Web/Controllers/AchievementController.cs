d8e197b61a2a935c368f0661530251bca938ced3
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-19 10:29:49 +0300

    Добавил просмотр таблицы **Achievement** при помощи `MVC`

---
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/AchievementController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Term5_RPBDIS_library.models.tables;
+using Term5_RPBDIS_mainLogic.Services;
+
+namespace Term5_RPBDIS_Web.Controllers {
+    public class AchievementController : Controller {
+        public IActionResult ShowTable([FromServices] AchievementService service) {
+            ViewBag.data = service.Get("Achievement20");
+
+            return View();
+        }
+    }
+}
--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Program.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Program.cs
@@ -1,25 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.IdentityModel.Tokens;
-using System.Reflection.Metadata;
 using Term5_RPBDIS_library;
-using Term5_RPBDIS_library.models.tables;
-using Term5_RPBDIS_mainLogic;
 using Term5_RPBDIS_mainLogic.Services;
-using Term5_RPBDIS_sql_library;
 
 const int CACHE_TIME_SECONDS = 264;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-//builder.Services.AddRazorPages();
-
 builder.Services.AddDbContext<ValuatingSystemContext>();
 builder.Services.AddDistributedMemoryCache();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(); // Add MVC to DI.
 builder.Services.AddSession();
+
 builder.Services.Configure<CookiePolicyOptions>(options =>
     options.CheckConsentNeeded = context => false
 );
@@ -40,38 +31,18 @@ builder.Services.AddTransient(x =>
 
 var app = builder.Build();
 
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseSession();
 
-app.UseRouting();
-
 app.UseAuthorization();
-
-//app.MapRazorPages();
-
-app.Map("/info", Middlewares.Info.ShowClientInfo);
-app.Map("/Achievement", Middlewares.Tables.ShowAchievement);
-app.Map("/Date", Middlewares.Tables.ShowDate);
-app.Map("/Division", Middlewares.Tables.ShowDivision);
-app.Map("/Employee", Middlewares.Tables.ShowEmployee);
-app.Map("/Mark", Middlewares.Tables.ShowMark);
-app.Map("/PlannedEfficiency", Middlewares.Tables.ShowPlannedEfficiency);
-app.Map("/RealEfficiency", Middlewares.Tables.ShowRealEfficiency);
-app.Map("/searchform1", Middlewares.Search.ShowForm1);
-app.Map("/searchform2", Middlewares.Search.ShowForm2);
 
 // MVC here!
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "Achievement",
+    pattern: "{controller=Achievement}/{action=ShowTable}");
 
 app.Run();
