d7980449274bb87cf41e74bfd7dd3124ead360f7
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-19 18:08:26 +0300

    Удалил все ненужное и немного отрефакторил код.

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Program.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Program.cs
@@ -3,46 +3,30 @@ using Term5_RPBDIS_library;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ValuatingSystemContext>();
-builder.Services.AddDistributedMemoryCache();
 builder.Services.AddControllersWithViews(); // Add MVC to DI.
 builder.Services.AddSession();
 builder.Services.AddResponseCaching();
 
-builder.Services.Configure<CookiePolicyOptions>(options =>
-    options.CheckConsentNeeded = context => false
-);
-
 var app = builder.Build();
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 app.UseSession();
 app.UseResponseCaching();
-app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapControllerRoute(
-    name: "Achievement",
-    pattern: "{controller=Achievement}/{action=ShowTable}");
-app.MapControllerRoute(
-    name: "Date",
-    pattern: "{controller=Date}/{action=ShowTable}");
-app.MapControllerRoute(
-    name: "Division",
-    pattern: "{controller=Division}/{action=ShowTable}");
-app.MapControllerRoute(
-    name: "Employee",
-    pattern: "{controller=Employee}/{action=ShowTable}");
-app.MapControllerRoute(
-    name: "Mark",
-    pattern: "{controller=Mark}/{action=ShowTable}");
-app.MapControllerRoute(
-    name: "PlannedEfficiency",
-    pattern: "{controller=PLannedEfficiency}/{action=ShowTable}");
-app.MapControllerRoute(
-    name: "RealEfficiency",
-    pattern: "{controller=RealEfficiency}/{action=ShowTable}");
+
+CreateTableRoutes();
 
 app.Run();
+
+void CreateTableRoutes() {
+    string[] controllers = { "Achievemtnt", "Date", "Division", "Employee", "Mark", "PlannedEfficiency", "RealEfficiency" };
+
+    foreach (var controller in controllers) {
+        app.MapControllerRoute(
+            name: controller,
+            pattern:  $"{{controller={controller}}}/{{action=ShowTable}}");
+    }
+}
