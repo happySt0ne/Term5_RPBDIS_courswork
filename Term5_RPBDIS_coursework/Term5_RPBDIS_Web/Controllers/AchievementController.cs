df61eee304fe4197dead7dfc34980f96f44b4fd3
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-20 14:11:08 +0300

    Добавлена операция **Create**

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/AchievementController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/AchievementController.cs
@@ -1,5 +1,28 @@
-﻿using Term5_RPBDIS_library.models.tables;
+﻿using Microsoft.AspNetCore.Mvc;
+using Term5_RPBDIS_library;
+using Term5_RPBDIS_library.models.tables;
 
 namespace Term5_RPBDIS_Web.Controllers {
-    public class AchievementController : ExpandedController<Achievement> { }
+    public class AchievementController : ExpandedController<Achievement> {
+        public AchievementController([FromServices] ValuatingSystemContext context) : base(context) { }
+
+        public override IActionResult Create() {
+            if (!TryGetFromQuery("Text", out string? text)) {
+                
+                return View();
+            }
+
+            Achievement achievement = new() {
+                Text = text
+            };
+
+            AddToDb(achievement);
+
+            return View();
+        }
+
+        public override IActionResult Update() {
+            throw new NotImplementedException();
+        }
+    }
 }
