9c2948de20a7ac8caf2f316f9ab0ffe20f99ce5d
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-20 14:04:32 +0300

    Добавлены абстрактные классы *CRUD*-операций, реализована общая операция удаления для всех контроллеров.

--- Term5_RPBDIS_coursework/Term5_RPBDIS_mainLogic/ExpandedController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_mainLogic/ExpandedController.cs
@@ -1,16 +1,77 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
 using Term5_RPBDIS_library;
 using Term5_RPBDIS_sql_library;
 
 namespace Term5_RPBDIS_Web.Controllers {
     public abstract class ExpandedController<T> : Controller where T : class, ISqlTable {
         private const int CacheDuration = 264;
+        protected ValuatingSystemContext _context;
 
-        [ResponseCache(Duration = CacheDuration)]
-        public IActionResult ShowTable([FromServices] ValuatingSystemContext valuatingSystemContext) {
-            ViewBag.data = valuatingSystemContext.Set<T>().ToList();
+        protected ExpandedController([FromServices] ValuatingSystemContext context) {
+            _context = context;
+        }
+
+        public abstract IActionResult Create();
+        
+        public IActionResult Delete() {
+            if (!TryGetFromQuery("Id", out int? id)) {
+
+                return View();
+            }
+
+            DeleteFromDb(id);
 
             return View();
+        }
+
+        public abstract IActionResult Update();
+
+        [ResponseCache(Duration = CacheDuration)]
+        public IActionResult ShowTable() {
+            ViewBag.data = _context.Set<T>().ToList();
+
+            return View();
+        }
+
+        protected void DeleteFromDb(int? id) {
+            T? entity = _context.Set<T>().Find(id);
+
+            if (entity is null) return;
+
+            DeleteFromDb(entity);
+        }
+
+        protected void DeleteFromDb(T recordToDelete) {
+            _context.Set<T>().Remove(recordToDelete);
+            _context.SaveChanges();
+        }
+
+        protected void AddToDb(T recordToAdd) {
+            _context.Set<T>().Add(recordToAdd);
+            _context.SaveChanges();
+        }
+
+        protected bool TryGetFromQuery(string key, out int? number) {
+            if (!HttpContext.Request.Query.ContainsKey(key)) {
+
+                number = null;
+                return false;
+            }
+
+            number = int.Parse(HttpContext.Request.Query[key]);
+            return true;
+        }
+
+        protected bool TryGetFromQuery(string key, out string? str) {
+            if (!HttpContext.Request.Query.ContainsKey(key)) {
+
+                str = null;
+                return false;
+            }
+
+            str = HttpContext.Request.Query[key];
+            return true;
         }
     }
 }
