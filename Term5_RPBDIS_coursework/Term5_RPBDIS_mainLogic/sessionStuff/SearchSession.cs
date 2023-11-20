bc46fb9ad3a41275771fe67c67c14d5e58abaa79
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-20 05:43:39 +0300

    Для удобства добавил метод для сохранения данных в сессии.

--- Term5_RPBDIS_coursework/Term5_RPBDIS_mainLogic/sessionStuff/SearchSession.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_mainLogic/sessionStuff/SearchSession.cs
@@ -1,8 +1,19 @@
-﻿namespace Term5_RPBDIS_mainLogic.sessionStuff {
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Term5_RPBDIS_mainLogic.sessionStuff {
     public class SearchSession {
         public string tableName;
         public string columnName;
         public string textForSearch;
         public bool isSaved = false;
+
+        public void Save(string table, string column, string text, HttpContext context) {
+            tableName = table;
+            columnName = column;
+            textForSearch = text;
+            isSaved = true;
+
+            context.Session.Set("searchSession", this);
+        }
     }
 }
