9d1b78ff393b6a9a991db610d872b22ef4f49489
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-21 15:02:34 +0300

    Добавил представление для создания.

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/RealEfficiencyController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/RealEfficiencyController.cs
@@ -4,11 +4,33 @@ using Term5_RPBDIS_library.models.tables;
 
 namespace Term5_RPBDIS_Web.Controllers {
     public class RealEfficiencyController : ExpandedController<RealEfficiency> {
-        public RealEfficiencyController([FromServices] ValuatingSystemContext context) : base(context) {
-        }
+        public RealEfficiencyController(ValuatingSystemContext context) : base(context) { }
 
         public override IActionResult Create() {
-            throw new NotImplementedException();
+            if (!TryGetFromQuery("Efficiency", out int? efficiency) ||
+                !TryGetFromQuery("StartDate", out DateTime? startDate) ||
+                !TryGetFromQuery("EndDate", out DateTime? endDate)) {
+
+                return View();
+            }
+
+            if (!IsRecordExist<Date>(x => x.EndDate == endDate && x.StartDate == startDate, out int? dateId)) {
+                Date date = new() {
+                    StartDate = startDate,
+                    EndDate = endDate
+                };
+
+                dateId = AddToDb(date);
+            }
+
+            RealEfficiency realEfficiency = new() {
+                DateId = dateId,
+                Efficiecy = efficiency,
+            };
+
+            AddToDb(realEfficiency);
+
+            return View();
         }
 
         public override IActionResult Update() {
