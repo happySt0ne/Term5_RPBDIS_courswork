e33192255b6df9731067df3a9316b80e7cda085d
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-21 14:52:05 +0300

    Добавил представление для создания нового плана.

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/PlannedEfficiencyController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/PlannedEfficiencyController.cs
@@ -4,13 +4,35 @@ using Term5_RPBDIS_library.models.tables;
 
 namespace Term5_RPBDIS_Web.Controllers {
     public class PlannedEfficiencyController : ExpandedController<PlannedEfficiency> {
-        public PlannedEfficiencyController([FromServices] ValuatingSystemContext context) : base(context) {
-        }
+        public PlannedEfficiencyController(ValuatingSystemContext context) : base(context) { }
 
         public override IActionResult Create() {
-            throw new NotImplementedException();
-        }
+            if (!TryGetFromQuery("Efficiency", out int? efficiency) ||
+                !TryGetFromQuery("StartDate", out DateTime? startDate) ||
+                !TryGetFromQuery("EndDate", out DateTime? endDate)) {
 
+                return View();
+            }
+
+            if (!IsRecordExist<Date>(x => x.EndDate == endDate && x.StartDate == startDate, out int? dateId)) {
+                Date date = new() {
+                    StartDate = startDate,
+                    EndDate = endDate
+                };
+
+                dateId = AddToDb(date);
+            }
+            
+            PlannedEfficiency plannedEfficiency = new() {
+                DateId = dateId,
+                Efficiecy = efficiency,
+            };
+
+            AddToDb(plannedEfficiency);
+
+            return View();
+        }
+    
         public override IActionResult Update() {
             throw new NotImplementedException();
         }
