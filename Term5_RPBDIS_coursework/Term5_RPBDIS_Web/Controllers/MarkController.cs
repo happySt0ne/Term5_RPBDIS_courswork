6bd7211e39fa821a0b693316f5b6f2166f719493
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-21 13:06:08 +0300

    Добавил представление для создания оценок.

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/MarkController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/MarkController.cs
@@ -4,11 +4,21 @@ using Term5_RPBDIS_library.models.tables;
 
 namespace Term5_RPBDIS_Web.Controllers {
     public class MarkController : ExpandedController<Mark> {
-        public MarkController([FromServices] ValuatingSystemContext context) : base(context) {
-        }
+        public MarkController(ValuatingSystemContext context) : base(context) { }
 
         public override IActionResult Create() {
-            throw new NotImplementedException();
+            if (!TryGetFromQuery("mark", out int? value)) {
+
+                return View();
+            }
+
+            Mark mark = new() {
+                Value = value
+            };
+
+            AddToDb(mark);
+
+            return View();
         }
 
         public override IActionResult Update() {
