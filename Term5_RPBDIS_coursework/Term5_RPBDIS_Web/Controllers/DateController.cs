67cc1ce184c011799bea16471cf2addf35725113
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-21 12:54:54 +0300

    Добавил представление для создания дат.

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/DateController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/DateController.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 using Term5_RPBDIS_library;
 using Term5_RPBDIS_library.models.tables;
 
 namespace Term5_RPBDIS_Web.Controllers {
     public class DateController : ExpandedController<Date> {
-        public DateController([FromServices] ValuatingSystemContext context) : base(context) {
-        }
+        public DateController([FromServices] ValuatingSystemContext context) : base(context) { }
 
         public override IActionResult Create() {
-            throw new NotImplementedException();
+            if (!TryGetFromQuery("StartDate", out string? startDate) || 
+                !TryGetFromQuery("EndDate", out string? endDate)) {
+
+                return View();
+            }
+
+            Date date = new() {
+                StartDate = DateTime.ParseExact(startDate, "yyyy-MM-dd", CultureInfo.InvariantCulture),
+                EndDate = DateTime.ParseExact(endDate, "yyyy-MM-dd", CultureInfo.InvariantCulture)
+            };
+
+            AddToDb(date);
+
+            return View();
         }
 
         public override IActionResult Update() {
