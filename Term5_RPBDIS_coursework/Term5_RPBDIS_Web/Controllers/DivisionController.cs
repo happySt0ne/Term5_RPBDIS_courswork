d93d3775c0484efa41786206763d10fafae526b7
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-28 01:11:45 +0300

    Добавил форму для изменения записей таблицы **Division**

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/DivisionController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/DivisionController.cs
@@ -21,7 +21,35 @@ namespace Term5_RPBDIS_Web.Controllers {
         }
 
         public override IActionResult Update() {
-            throw new NotImplementedException();
+            ViewBag.Divisions = _context.Divisions.ToList();
+            ViewBag.PlannedEfficiencies = _context.PlannedEfficiencies.ToList();
+            ViewBag.RealEfficiencies = _context.RealEfficiencies.ToList();
+            ViewBag.Marks = _context.Marks.ToList();
+
+            if (!TryGetFromQuery("Id", out int? id)) return View();
+
+            Division division = _context.Divisions.Find(id);
+
+            if (TryGetFromQuery("Name", out string? name)) {
+                
+                division.Name = name;
+            }
+            if (TryGetFromQuery("MarkId", out int? markId)) {
+
+                division.MarkId = markId;
+            }
+            if (TryGetFromQuery("PlannedEfficiencyId", out int? plannedEfficiencyId)) {
+
+                division.PlannedEfficiencyId = plannedEfficiencyId;
+            }
+            if (TryGetFromQuery("RealEfficiencyId", out int? realEfficiencyId)) {
+
+                division.RealEfficiencyId = realEfficiencyId;
+            }
+
+            _context.SaveChanges();
+
+            return View();
         }
     }
 }
