04700990117ddc0f42e28d43a253db19b99ab8e6
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-28 02:05:32 +0300

    Добавил форму для изменения **Employee**

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/EmployeeController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/EmployeeController.cs
@@ -4,8 +4,7 @@ using Term5_RPBDIS_library.models.tables;
 
 namespace Term5_RPBDIS_Web.Controllers {
 	public class EmployeeController : ExpandedController<Employee> {
-		public EmployeeController([FromServices] ValuatingSystemContext context) : base(context) {
-		}
+		public EmployeeController(ValuatingSystemContext context) : base(context) { }
 
 		public override IActionResult Create() {
 			if (TryGetFromQuery("Name", out string? name) &&
@@ -26,7 +25,40 @@ namespace Term5_RPBDIS_Web.Controllers {
 		}
 
 		public override IActionResult Update() {
-			throw new NotImplementedException();
+			ViewBag.Employees = _context.Employees.ToList();
+			ViewBag.Divisions = _context.Divisions.ToList();
+			ViewBag.Achievements = _context.Achievements.ToList();
+			ViewBag.Marks = _context.Marks.ToList();
+
+			if (!TryGetFromQuery("Id", out int? id)) return View();
+
+
+			Employee employee = _context.Employees.Find(id);
+
+			if (TryGetFromQuery("Name", out string? name)) {
+
+				employee.Name = name;
+			}
+			if (TryGetFromQuery("DivisionId", out int? divisionId)) {
+
+				employee.DivisionId = divisionId;
+			}
+			if (TryGetFromQuery("HireDate", out DateTime? hireDate)) {
+
+				employee.HireDate = hireDate;
+			}
+			if (TryGetFromQuery("AchievementId", out int? achievementId)) {
+
+				employee.AchievementId = achievementId;
+			}
+			if (TryGetFromQuery("MarkId", out int? markId)) {
+
+				employee.MarkId = markId;
+			}
+
+			_context.SaveChanges();
+
+			return View();
 		}
 	}
 }
