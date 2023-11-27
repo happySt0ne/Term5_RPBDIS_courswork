a4cfcbe7367e85985408159628d5d8dc46e14e5b
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-27 23:45:59 +0300

    Добавил форму для изменения **RealEfficiency**

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/RealEfficiencyController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/RealEfficiencyController.cs
@@ -18,7 +18,26 @@ namespace Term5_RPBDIS_Web.Controllers {
         }
 
         public override IActionResult Update() {
-            throw new NotImplementedException();
+            ViewBag.RealEfficiency = _context.RealEfficiencies.ToList();
+            ViewBag.Dates = _context.Dates.ToList();
+
+            if (!TryGetFromQuery("Id", out int? id)) return View();
+
+            RealEfficiency realEfficiencies = _context.RealEfficiencies.Find(id);
+
+            if (TryGetFromQuery("Date", out int? dateId)) {
+
+                realEfficiencies.Date = _context.Dates.Find(dateId);
+            }
+
+            if (TryGetFromQuery("Efficiecy", out int? efficiecy)) {
+
+                realEfficiencies.Efficiecy = efficiecy;
+            }
+
+            _context.SaveChanges();
+
+            return View();
         }
     }
 }
