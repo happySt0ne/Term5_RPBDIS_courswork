9f1f1afe085b3129810bf3a1ef3214d3585a1587
H1Kt0 <konovalchuk.denis@gmail.com>
2023-11-27 14:09:50 +0300

    Добавил форму для изменения записей таблицы **Achievement**

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/AchievementController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/AchievementController.cs
@@ -4,7 +4,7 @@ using Term5_RPBDIS_library.models.tables;
 
 namespace Term5_RPBDIS_Web.Controllers {
     public class AchievementController : ExpandedController<Achievement> {
-        public AchievementController([FromServices] ValuatingSystemContext context) : base(context) { }
+        public AchievementController(ValuatingSystemContext context) : base(context) { }
 
         public override IActionResult Create() {
             if (TryGetFromQuery("Text", out string? text)) {
@@ -16,7 +16,20 @@ namespace Term5_RPBDIS_Web.Controllers {
         }
 
         public override IActionResult Update() {
-            throw new NotImplementedException();
+            ViewBag.Achievements = _context.Achievements.ToList();
+
+            if (!TryGetFromQuery("Id", out int? id)) return View();
+            
+            Achievement achievement = _context.Achievements.Find(id);
+            
+            if (TryGetFromQuery("Text", out string? text)) {
+                
+                achievement.Text = text;
+            }
+            
+            _context.SaveChanges();
+
+            return View();
         }
     }
 }
