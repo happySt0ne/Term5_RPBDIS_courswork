0209cfa53b371e7193aa1a8caab4d082c320999b
H1Kt0 <konovalchuk.denis@gmail.com>
2023-12-06 02:38:45 +0300

    Добавлена регистрация. (До тестов и правок.)

--- Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/AccountController.cs
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/Controllers/AccountController.cs
@@ -1,19 +1,51 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using Term5_RPBDIS_Web.ViewModels;
 
 namespace Term5_RPBDIS_Web.Controllers {
+    // TODO: попробуй потом ограничить данные на странице Index. Типа для админа пусть там будет ещё вкладка для управления учетными записями пользователей.
     public class AccountController : Controller {
-        [Authorize]
-        public IActionResult Index() {
-            return View();
+        private UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager) {
+            _userManager = userManager;
         }
-        public IActionResult Login() {
+
+        public async Task<IActionResult> UsersList() {
+            ViewBag.Users = await _userManager.Users.ToListAsync();
+
             return View();
         }
 
-        public IActionResult Register() {
-            return View();
+        public IActionResult Login() => View();
+
+        [HttpGet]
+        public IActionResult Register() => View();
+        
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model) {
+            var user = new IdentityUser { PhoneNumber = model.PhoneNumber };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded) {
+                foreach (var error in result.Errors) {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model); // Возвращение на страницу регистрации с сообщениями об ошибках
+            }
+
+            // Тут менять буду. 
+            if (_userManager.Users.Count() == 1) {
+                await _userManager.AddToRoleAsync(user, "Admin");
+            } else {
+                await _userManager.AddToRoleAsync(user, "User");
+            }
+            return RedirectToAction("Index", "Home"); // Перенаправление на главную страницу после успешной регистрации
         }
+
     }
 }
---
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Term5_RPBDIS_Web.ViewModels {
+    public class RegisterViewModel {
+        [Required]
+        [Phone]
+        [Display(Name = "PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Подтвердите пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
