5f434d820a2e2bd235945de019a900b2b9b09689
H1Kt0 <konovalchuk.denis@gmail.com>
2023-12-19 23:50:46 +0300

    Вынес скрипт кнопок для получения id выбранной записи в отдельный файл.

---
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/wwwroot/js/ShowTable.js
@@ -0,0 +1,7 @@
+﻿
+    document.querySelectorAll(".editButton").forEach(function (button) {
+        button.addEventListener("click", function () {
+            var id = this.getAttribute("data-id");
+            document.getElementById("Id").value = id;
+        });
+        });
