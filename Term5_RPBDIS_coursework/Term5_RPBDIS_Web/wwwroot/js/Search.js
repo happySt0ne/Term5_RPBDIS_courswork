63ef36282c9e03af931429605ababe15e77342de
Денис Коновальчук <103820240+happySt0ne@users.noreply.github.com>
2023-12-25 03:35:32 +0300

    Фикс багов поиска (#28)
    * Исправлены баги отображения неправильной таблицы.
    
    * Исправлены баги когда поиск по некоторым таблицам не работал.

---
+++ Term5_RPBDIS_coursework/Term5_RPBDIS_Web/wwwroot/js/Search.js
@@ -0,0 +1,5 @@
+﻿window.onload = function () {
+    // Считывание значения из скрытого элемента
+    var tableValue = document.getElementById("tableValue").value;
+    document.getElementById("choosingList").value = tableValue;
+}
